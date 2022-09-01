c3c25e8b6fa12ba7132adf156b839cefe5951442
d3n <d3n@d3n.it>
2022-09-01 20:11:42 +0200

    Fix render

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/d3n972/mavint
 go 1.19
 
 require (
+	github.com/foolin/goview v0.3.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"html/template"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/d3n972/mavint/controllers"
+	"github.com/foolin/goview"
+	"github.com/foolin/goview/supports/ginview"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,63 +22,71 @@ var assets embed.FS
 func main() {
 	r := gin.Default()
 
-	//templ := template.Must(template.New("").ParseFS(templates, "templates/*.tmpl"))
-
-	templ := template.Must(template.New("").Funcs(template.FuncMap{
-		"toIconName": func(trainType string) string {
-			t := strings.ToLower(trainType)
-			switch t {
-			case "euronight":
-				return "euronight"
-			case "expresszvonat":
-				return "expresszvonat"
-			case "gyorsvonat":
-				return "gyorsvonat"
-			case "intercity":
-				return "intercity"
-			case "interregio":
-				return "interregio"
-			case "railjet":
-				return "railjet"
-			case "sebesvonat":
-				return "sebesvonat"
-			case "személyvonat":
+	r.HTMLRender = ginview.New(goview.Config{
+		Root:      "templates",
+		Extension: ".tmpl",
+		Master:    "layouts/master",
+		Funcs: template.FuncMap{
+			"getColorOrFallback": func(a *string, b *string) string {
+				if a == nil {
+					return *b
+				}
+				return *a
+			},
+			"toIconName": func(trainType string) string {
+				t := strings.ToLower(trainType)
+				switch t {
+				case "euronight":
+					return "euronight"
+				case "expresszvonat":
+					return "expresszvonat"
+				case "gyorsvonat":
+					return "gyorsvonat"
+				case "intercity":
+					return "intercity"
+				case "interregio":
+					return "interregio"
+				case "railjet":
+					return "railjet"
+				case "sebesvonat":
+					return "sebesvonat"
+				case "személyvonat":
+					return "szemelyvonat"
+				case "euregio":
+					return "euregio"
+				case "eurocity":
+					return "eurocity"
+				}
 				return "szemelyvonat"
-			case "euregio":
-				return "euregio"
-			case "eurocity":
-				return "eurocity"
-			}
-			return "szemelyvonat"
+			},
+			"toTrainType": func(t string) string {
+				switch t {
+				case "BESZ":
+					return ""
+				case "BEG":
+					return "G"
+				case "BEZ":
+					return "Z"
+				default:
+					return t
+				}
+			},
+			"loctime": func(x time.Time) string {
+				return x.Local().Format(time.Kitchen)
+			},
 		},
-		"toTrainType": func(t string) string {
-			switch t {
-			case "BESZ":
-				return ""
-			case "BEG":
-				return "G"
-			case "BEZ":
-				return "Z"
-			default:
-				return t
-			}
-		},
-		"loctime": func(x time.Time) string {
-			return x.Local().Format(time.Kitchen)
-		},
-	}).ParseGlob("templates/*.tmpl"))
-	fmt.Printf("templ.DefinedTemplates(): %+v\n", templ.DefinedTemplates())
-	r.SetHTMLTemplate(templ)
-	//r.LoadHTMLGlob("templates/*")
-	// example: /public/assets/images/example.png
+		DisableCache: true,
+		Delims:       goview.Delims{Left: "{{", Right: "}}"},
+	})
+
 	r.StaticFS("/public", http.FS(assets))
 	ttblCtrl := controllers.TimetableController{}
-	tdCtrl := controllers.TrainDetailsController{}
+	//tdCtrl := controllers.TrainDetailsController{}
 	r.GET("/tt", ttblCtrl.Render)
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 	})
-	r.GET("/m", tdCtrl.Render)
+	//r.GET("/m", tdCtrl.Render)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
