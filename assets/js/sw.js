b3737930a565232669ca016595a1ff0dca5eecb8
d3n <d3n@d3n.it>
2022-09-11 21:25:00 +0200

    Add service worker

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,31 +1,19 @@
-const cacheName = 'js13kPWA-v1';
+const cacheName = 'mavtc-v1';
 const appShellFiles = [
-    '/pwa-examples/js13kpwa/',
-    '/pwa-examples/js13kpwa/index.html',
-    '/pwa-examples/js13kpwa/app.js',
-    '/pwa-examples/js13kpwa/style.css',
-    '/pwa-examples/js13kpwa/fonts/graduate.eot',
-    '/pwa-examples/js13kpwa/fonts/graduate.ttf',
-    '/pwa-examples/js13kpwa/fonts/graduate.woff',
-    '/pwa-examples/js13kpwa/favicon.ico',
-    '/pwa-examples/js13kpwa/img/js13kgames.png',
-    '/pwa-examples/js13kpwa/img/bg.png',
-    '/pwa-examples/js13kpwa/icons/icon-32.png',
-    '/pwa-examples/js13kpwa/icons/icon-64.png',
-    '/pwa-examples/js13kpwa/icons/icon-96.png',
-    '/pwa-examples/js13kpwa/icons/icon-128.png',
-    '/pwa-examples/js13kpwa/icons/icon-168.png',
-    '/pwa-examples/js13kpwa/icons/icon-192.png',
-    '/pwa-examples/js13kpwa/icons/icon-256.png',
-    '/pwa-examples/js13kpwa/icons/icon-512.png'
+    '/public/assets/app-192x.png',
+    '/public/assets/app-512x.png',
+    '/public/assets/mnr.css',
+    '/public/assets/MNR2007.cd059e2cbf346028f917.woff2',
+    '/public/assets/stations.json',
+    '/public/assets/train_types.svg',
+    '/public/assets/trainclasses.svg',
 ];
-const contentToCache = appShellFiles.concat(gamesImages);
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil((async () => {
         const cache = await caches.open(cacheName);
         console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(contentToCache);
+        await cache.addAll(appShellFiles);
     })());
 });
 self.addEventListener('fetch', (e) => {
